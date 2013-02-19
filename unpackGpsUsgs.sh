a8b4ae869d251d5bf2784f4f124de54e7d0c06d8
James L McCreight <mccreigh@gmail.com>
2013-02-18 23:12:43 -0700

    fourth commit with updated readme: share with world this poorly documented/exampled task

--- unpackGpsUsgs.sh
+++ unpackGpsUsgs.sh
@@ -4,12 +4,12 @@ while read line
 do           
     ## echo -e $line
     line2=`echo $line | cut -d '.' -f 1`
-    ##echo /bowie/scratch/mccreigh/USGS/zipped/${line2}.zip -d /bowie/scratch/mccreigh/USGS/unzipped/gps/${line2}/
+    ##echo /server/scratch/mccreigh/USGS/zipped/${line2}.zip -d /server/scratch/mccreigh/USGS/unzipped/gps/${line2}/
     if [[ $line2 == n* ]]
     then  ## dont have to protect ned products from exploding
-	unzip /bowie/scratch/mccreigh/USGS/zipped/${line2}.zip -d /bowie/scratch/mccreigh/USGS/unzipped/gps/
+	unzip /server/scratch/mccreigh/USGS/zipped/${line2}.zip -d /server/scratch/mccreigh/USGS/unzipped/gps/
     else ## do have to protect NLCD from exploding
-	unzip /bowie/scratch/mccreigh/USGS/zipped/${line2}.zip -d /bowie/scratch/mccreigh/USGS/unzipped/gps/${line2}/
+	unzip /server/scratch/mccreigh/USGS/zipped/${line2}.zip -d /server/scratch/mccreigh/USGS/unzipped/gps/${line2}/
     fi 
 
 done <~/density/gps/gpsUsgsFileNamesNoAK.txt
